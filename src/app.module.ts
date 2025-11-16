c1014b75ce29d4dce5fbcab8c97963b7588c9432
gixxla <gigixquisite@gmail.com>
2025-11-17 04:39:12 +0900

    Feat: DB 연동
    - DB 연동 및 schema scripts 추가
    - user 모듈 파일 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './res/user/user.module';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST!,
+        port: parseInt(process.env.DB_PORT!),
+        username: process.env.DB_USERNAME!,
+        password: process.env.DB_PASSWORD!,
+        database: process.env.DB_NAME!,
+        entities: [path.join(__dirname, '**', '*.entity.{ts,js}')],
+        synchronize: false,
+        logging: true,
+        timezone: 'local',
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
